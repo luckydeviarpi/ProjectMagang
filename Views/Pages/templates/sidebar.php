1b6cfe005bb3c47d0787714e8eca61c7f02307b9
luckydeviarpi <lucky22ti@mahasiswa.pcr.ac.id>
2023-06-29 13:30:50 +0700

    datatable masih masalah

---
+++ Views/Pages/templates/sidebar.php
@@ -0,0 +1,51 @@
+<div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="dropdown">
+              <a href="#" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            
+            </li>
+          
+            <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Lowongan Magang</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="layout-default.html">Terbuka</a></li>
+                <li><a class="nav-link" href="layout-transparent.html">Selesai</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-th"></i> <span>Kelola</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="layout-default.html">Ajukan Lowongan</a></li>
+                <li><a class="nav-link" href="layout-transparent.html">Pengajuan Lowongan</a></li>
+                <li><a class="nav-link" href="layout-transparent.html">Lowongan Anda</a></li>
+                <li><a class="nav-link" href="layout-transparent.html">Magang Aktif</a></li>
+              </ul>
+            </li>
+          
+    
+            <li class="menu-header">Pages</li>
+            <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
+              <ul class="dropdown-menu">
+                <li><a href="auth-forgot-password.html">Forgot Password</a></li> 
+                <li><a href="auth-login.html">Login</a></li> 
+                <li><a href="auth-register.html">Register</a></li> 
+                <li><a href="auth-reset-password.html">Reset Password</a></li> 
+              </ul>
+            </li>
+           </ul>
+
+          <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+              <i class="fas fa-rocket"></i> Magang
+            </a>
+          </div>        </aside>
+      </div>
